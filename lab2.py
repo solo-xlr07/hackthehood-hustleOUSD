3d6a156d673e42eb398656b90e63e70bfb8306f7
Nelson Perez <nelsonperez@Nelsons-Air.attlocal.net>
2025-06-18 13:32:24 -0700

    conpleted lab2

---
+++ lab2.py
@@ -0,0 +1,24 @@
+#LAB 2 
+
+#step 1
+name = 'Nelson Perez'
+print(name)
+
+#step 2
+nickame = 'Teto'
+
+#step 3
+age = 17
+
+#step 4
+used_python = True
+
+#step 5
+hobbies = ['soccer', 'playing videogames', 
+'sleeping', "playing cajun", "art", 'babysitting']
+
+#step 6
+favorite_things = {"movie":"Baby Driver", 'food':'pupusas', 
+'color':'light blue', 'animal':'Allosaurs', 'sport':'soccer',}
+
+#Optional: add your own variables here!
